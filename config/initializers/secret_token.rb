f3a043f1f64e1e61a8ddd5e61812ae3b53accd49
Jacob <hall.j.alexander@gmail.com>
2013-07-30 17:08:57 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trotter::Application.config.secret_key_base = '4aff7f497ecca8bd735a9699c2304928cfb7c7cf00a5da605a208827fda601fd7ff218f0bd08b36ddde958466948da285df738c92a3a06a2b766708f221aa1f2'
